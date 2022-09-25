8c1aae6035de3da967804a8cab9e6b3f5d0f28f2
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-09-24 22:15:23 -0300

    style(Home): call to action

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import style from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className={style.container}>
       <Head>
         <title>TaskYup</title>
         <meta name="description" content="Lorem ipsum" />
@@ -12,12 +13,42 @@ const Home: NextPage = () => {
       </Head>
 
       <header>
-        <Image src="/taskyup.svg" alt="Logo TaskYup" width={150} height={50} />
+        <Image src="/taskyup.svg" alt="Logo TaskYup" width={100} height={25} />
+
+        <nav>
+          <a
+            target="_blank"
+            href="https://app.taskyup.com/auth/signin"
+            rel="noopener noreferrer"
+          >
+            Sign in
+          </a>
+          <a
+            target="_blank"
+            href="https://app.taskyup.com/auth/signup"
+            rel="noopener noreferrer"
+          >
+            <button>Sign up</button>
+          </a>
+        </nav>
       </header>
 
       <main>
-        <div className="call-to-action-container">
-          <h1>New way<br /> to <span>organize</span></h1>
+        <div className={style.call_to_action_container}>
+          <h1>
+            New way
+            <br /> to <span>organize</span>
+          </h1>
+          <p>Easily and quickly organize your day</p>
+          <button>Get started</button>
+          <figure>
+            <Image
+              src="/background.png"
+              alt="Background TaskYup"
+              width={1034}
+              height={909}
+            />
+          </figure>
         </div>
       </main>
     </div>
